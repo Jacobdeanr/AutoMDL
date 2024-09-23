36ea7d1229d21195fbc8538f10b0442da25a6d45
unknown <jacobdeanr@gmail.com>
2024-09-23 18:44:05 -0500

    Forgot to push this

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 bl_info = {
     "name": "AutoMDL",
-    "author": "NvC_DmN_CH",
+    "author": "NvC_DmN_CH, Jacob Robbins",
     "version": (1, 1),
     "blender": (4, 0, 0),
     "location": "View3D > Sidebar > AutoMDL",
@@ -14,32 +14,51 @@ import bpy
 from .auto_mdl_panel import AutoMDLPanel
 from .auto_mdl_config import AutoMDLConfig, CdMaterialsPropGroup
 from .auto_mdl_operator import AutoMDLOperator
+from .constants import SURFACE_PROP_ITEMS
+from .utils import format_game_select_items
 
+# Global config instance
 config = AutoMDLConfig()
 
+def update_game_dropdown(self, context):
+    """Called when the game dropdown changes."""
+    config.ui_interaction_manager.on_game_dropdown_changed(context)
+
+def update_manual_input(self, context):
+    """Called when the manual game path input changes."""
+    config.ui_interaction_manager.on_game_manual_text_input_changed(context)
+
+def get_game_select_items(self, context):
+    """Get the items for the game select dropdown."""
+    config = AutoMDLConfig()
+    return format_game_select_items(config.game_path_manager.games_paths_list)
+
 class AddonPrefs(bpy.types.AddonPreferences):
-    bl_idname = __package__
+    """Addon preferences for AutoMDL settings."""
     
+    bl_idname = __package__
+
+    # User preferences
     do_make_folders_for_cdmaterials: bpy.props.BoolProperty(
         name="Make Folders",
-        description="On compile, make the appropriate folders in the materials folder (make folders for each $cdmaterials)",
+        description="Create appropriate folders for each $cdmaterials on compile",
         default=True
     )
     
     do_make_vmts: bpy.props.BoolProperty(
         name="Make placeholder VMTs",
-        description="On compile, make placeholder VMT files named after the model's materials, placed inside appropriate folder inside the materials folder\nThis won't replace existing VMTs",
+        description="Create placeholder VMT files for the model's materials inside the appropriate material folder. Will not overwrite existing VMTs.",
         default=True
     )
     
     def draw(self, context):
+        """Draw UI for addon preferences."""
         layout = self.layout
-        row = layout.row()
-        row.prop(self, "do_make_folders_for_cdmaterials", text="Automatically make folders for materials locations")
-        row = layout.row()
-        row.enabled = self.do_make_folders_for_cdmaterials
-        row.prop(self, "do_make_vmts", text="Also make placeholder VMTs (Only when compiling with the \"Same as MDL\" option)")
+        layout.prop(self, "do_make_folders_for_cdmaterials", text="Automatically create material folders")
+        layout.prop(self, "do_make_vmts", text="Create placeholder VMTs (Only with 'Same as MDL' option)")
+        layout.enabled = self.do_make_folders_for_cdmaterials
 
+# Classes to be registered/unregistered
 classes = [
     AutoMDLOperator,
     AutoMDLPanel,
@@ -48,16 +67,11 @@ classes = [
 ]
 
 def register():
-    from bpy.utils import register_class, unregister_class
+    """Register all classes and custom properties for the addon."""
+    from bpy.utils import register_class
+    
 
-    # Unregister classes if they are already registered
-    for cls in classes:
-        try:
-            unregister_class(cls)
-        except:
-            pass
-
-    # Register classes
+    # Register all classes
     for cls in classes:
         try:
             register_class(cls)
@@ -65,14 +79,16 @@ def register():
             print(f"Error registering class {cls.__name__}: {e}")
 
     # Register custom properties
-    config.register_custom_properties()
-
-    print("AutoMDL addon registered successfully")
+    register_properties()
+    
+    
+    print("AutoMDL addon registered successfully.")
 
 def unregister():
+    """Unregister all classes and custom properties."""
     from bpy.utils import unregister_class
 
-    # Unregister classes
+    # Unregister all classes
     for cls in reversed(classes):
         try:
             unregister_class(cls)
@@ -80,9 +96,63 @@ def unregister():
             print(f"Error unregistering class {cls.__name__}: {e}")
 
     # Unregister custom properties
-    config.unregister_custom_properties()
+    unregister_properties()
 
-    print("AutoMDL addon unregistered successfully")
+
+    print("AutoMDL addon unregistered successfully.")
+
+def unregister_properties():
+    """Unregister custom properties for the addon."""
+    del bpy.types.Scene.surfaceprop_text_input
+    del bpy.types.Scene.vis_mesh
+    del bpy.types.Scene.phy_mesh
+    del bpy.types.Scene.surfaceprop
+    del bpy.types.Scene.staticprop
+    del bpy.types.Scene.mostlyopaque
+    del bpy.types.Scene.mass_text_input
+    if config.game_path_manager.game_select_method_is_dropdown:
+        del bpy.types.Scene.game_select
+    else:
+        del bpy.types.Scene.studiomdl_manual_input
+    del bpy.types.Scene.cdmaterials_type
+    del bpy.types.Scene.cdmaterials_list
+    del bpy.types.Scene.cdmaterials_list_active_index
+
+def register_properties():
+    """Register custom properties for the addon."""
+    bpy.types.Scene.surfaceprop_text_input = bpy.props.StringProperty(name="", default="")
+    bpy.types.Scene.mass_text_input = bpy.props.StringProperty(
+        name="", default="35",
+        description="Mass in kilograms (KG). By default, the Player can +USE pick up 35KG max. The gravgun can pick up 250KG max. The portal gun can pick up 85KG max."
+    )
+    bpy.types.Scene.vis_mesh = bpy.props.PointerProperty(type=bpy.types.Object, name="Selected Object", description="Select an object from the scene")
+    bpy.types.Scene.phy_mesh = bpy.props.PointerProperty(type=bpy.types.Object, name="Selected Object", description="Select an object from the scene")
+    bpy.types.Scene.surfaceprop = bpy.props.EnumProperty(
+        name="Selected Option",
+        items=SURFACE_PROP_ITEMS
+    )
+    bpy.types.Scene.staticprop = bpy.props.BoolProperty(name="Static Prop", description="Enable if used as prop_static\n($staticprop in QC)", default=False)
+    bpy.types.Scene.mostlyopaque = bpy.props.BoolProperty(name="Has Transparency", description="Enabling this may fix sorting issues...", default=False)
+    bpy.types.Scene.cdmaterials_type = bpy.props.EnumProperty(items=(
+        ('0', 'Same as MDL', ''),
+        ('1', 'Other', '')
+    ))
+    bpy.types.Scene.cdmaterials_list = bpy.props.CollectionProperty(type=CdMaterialsPropGroup)
+    bpy.types.Scene.cdmaterials_list_active_index = bpy.props.IntProperty()
+
+    if config.game_path_manager.game_select_method_is_dropdown:
+        bpy.types.Scene.game_select = bpy.props.EnumProperty(
+            name="Select Game",
+            items=get_game_select_items,
+            update=update_game_dropdown  
+        )
+    else:
+        bpy.types.Scene.studiomdl_manual_input = bpy.props.StringProperty(
+            name="",
+            default="",
+            description="Path to the studiomdl.exe file",
+            update=update_manual_input  
+        )
 
 if __name__ == "__main__":
     register()
